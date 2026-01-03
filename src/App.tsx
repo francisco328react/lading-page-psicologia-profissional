67f3ad43f891ece70022a74800985266c55fb8ad
Francisco Neto <fseveriano56@hotmail.com>
2026-01-03 16:04:53 -0400

    chore: initial setup with Vite, React, TypeScript and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="min-h-screen bg-secondary text-gray-800">
+      <h1 className="text-center text-2xl mt-10 font-serif">
+        Beatriz Silveira Psicologia
+      </h1>
+    </div>
+  );
+}
